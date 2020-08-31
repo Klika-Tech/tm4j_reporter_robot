11ba1d4e6550bafd229cb1d555e4abf398aa5031
Yury Kuptsou <ykuptsou@iotecha.com>
2020-08-31 12:02:10 +0300

    Adding license header

--- tm4j_reporter_robot/TM4JRobotListener.py
+++ tm4j_reporter_robot/TM4JRobotListener.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2020 Klika Tech, Inc. or its affiliates.  All Rights Reserved.
+# Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
+# or at https://opensource.org/licenses/MIT.
+
 from datetime import datetime
 
 from tm4j_reporter_api.tm4j_api import tm4j_api

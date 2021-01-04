f1a493d604c4daa07e1a17720e3a3d174c61dccf
Yury Kuptsou <ykuptsou@iotecha.com>
2021-01-04 15:26:21 +0300

    Getting attrs from env

--- tm4j_reporter_robot/tm4j_robot_helpers/tm4j_test_cycle_helpers.py
+++ tm4j_reporter_robot/tm4j_robot_helpers/tm4j_test_cycle_helpers.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2020 Klika Tech, Inc. or its affiliates.  All Rights Reserved.
+# Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
+# or at https://opensource.org/licenses/MIT.
+
 import os
 
 from filelock import FileLock
